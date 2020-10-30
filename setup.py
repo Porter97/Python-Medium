634a8ee96a9f84b2bf6d4e5f9efbaf98abb17342
Luca Franceschini <luca.france@outlook.com>
2020-10-30 19:26:30 +0100

    Added long description for PyPI page

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "rt") as fh:
+    long_description = fh.read()
+
 setup(
     name='python-medium',
     version='v1.1',
@@ -9,6 +12,8 @@ setup(
     author='Spencer Porter',
     author_email='spencer.porter3@gmail.com',
     description='Python Medium API client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['requests'],
     download_url='https://github.com/Medium/medium-sdk-python/tarball/v0.3.0',
     keywords=['medium', 'sdk', 'oauth', 'api'],
